143ae45b9f20c2bb89bebdda0c29f2fd31765d1f
Eynar <yordanopv@gmail.com>
2024-06-08 23:13:07 -0600

    added delete function

--- routes/notes.js
+++ routes/notes.js
@@ -2,12 +2,28 @@ const notes = require('express').Router();
 const uuid = require('../helpers/uuid');
 
 // Helper functions for reading and writing to the JSON file
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
 
 // This API route is a GET Route for retrieving all the notes
 notes.get('/', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
+
+notes.delete('/:note_id', (req, res) => {
+    const noteId = req.params.note_id;
+    readFromFile('./db/db.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        // Make a new array of all notes except the one with the ID provided in the URL
+        const result = json.filter((note) => note.note_id !== noteId);
+  
+        // Save that array to the filesystem
+        writeToFile('./db/db.json', result);
+  
+        // Respond to the DELETE request
+        res.json(`Item ${noteId} has been deleted 🗑️`);
+      });
+  });
 
 
 module.exports = notes;
