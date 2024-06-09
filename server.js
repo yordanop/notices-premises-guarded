823c60930b0bb6f21da1e8a36d5b67ae002379ee
Eynar <yordanopv@gmail.com>
2024-06-08 22:45:53 -0600

    add middlware to GET notes

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const path = require('path');
+const api = require('./routes/index.js');
 
 const app =  express();
 
 const PORT = 3001;
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 
 // Invoke app.use() and serve static files from the '/public' folder
 app.use(express.static('public'));
@@ -15,9 +21,9 @@ app.get('/notes', (req, res) =>
   );
 
 // Wildcard
-app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/404.html'))
-  );
+// app.get('*', (req, res) =>
+//     res.sendFile(path.join(__dirname, '/public/404.html'))
+//   );
 
   app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
